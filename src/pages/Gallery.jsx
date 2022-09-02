0e1c251ed57be0bed9f40cc2313266ec643e0159
elijahthis <emperorelijah007@gmail.com>
2022-09-02 15:54:34 +0100

    gallery. all pages on ground

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,40 @@
 import { MainBanner } from "../components/MainBanner";
+import facility1 from "../assets/images/facility-1.png";
+import "./styles.scss";
 
-const Gallery = () => (
-    <div className="Gallery">
-        <MainBanner title={"Gallery"} />
-    </div>
-);
+const Gallery = () => {
+    const images = Array(9)
+        .fill()
+        .map((i) => facility1);
+
+    return (
+        <div className="Gallery">
+            <MainBanner title={"Gallery"} />
+            <div className="main-block">
+                <div className="image-batch">
+                    <div>
+                        {images.map((image) => (
+                            <img src={image} />
+                        ))}
+                    </div>
+                    <div className="button-wrap">
+                        <button>View More</button>
+                    </div>
+                </div>
+                <div className="image-batch">
+                    <h3>Boarding School Facilities</h3>
+                    <div>
+                        {images.map((image) => (
+                            <img src={image} />
+                        ))}
+                    </div>
+                    <div className="button-wrap">
+                        <button>View More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Gallery;
