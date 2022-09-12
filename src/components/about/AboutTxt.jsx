7bf18a8d46e222e05032d9a9fada618203bc5f32
Elijah Oyerinde <emperorelijah007.com>
2022-09-12 11:46:00 +0100

    finishing touches

--- src/components/about/AboutTxt.jsx
+++ src/components/about/AboutTxt.jsx
@@ -1,63 +1,68 @@
 import "./styles.scss";
+import { Flip, Fade } from "react-reveal";
 
 const AboutTxt = () => {
   return (
     <div className="AboutTxt">
-      <h2>ABOUT INTOUCH PRIVATE ACADEMY</h2>
-      <div>
-        <p>
-          Intouch Private Academy was established in January 2011 by the schools
-          proprietress and educationist, who is driven by the compulsion to see
-          younglings thrive and make a name for themselves. At Intouch Private
-          Academy every student is treated essentially as an individual, with
-          the potential for further excellence waiting to be explored and
-          refined. Our students are carefully guided through their adolescence
-          and taught to see mishaps and failures as success inclined, provided
-          that they learn from the negative experiences and also to encourage
-          the baby steps of progress taken by fellow students likewise. This
-          atmosphere encourages our students to develop a lasting sense of
-          moral, social and spiritual responsibility. The aim is to turn out
-          self-assured and caring people through association and the co-activity
-          of all school based help administrations. The school likewise has
-          prepared boarding facilities to house and really focus on her boarding
-          students:
-        </p>
-        <p className="left">
-          • House Parents are endowed with the responsibility of the wellbeing
-          of the students within the classroom and school context as well as
-          ensuring continuity of care and wellbeing of the wards, supported by
-          the resident medical staff, they also monitor assignments as well as
-          supervising evening weekend study time. They develop firm, meaningful,
-          nurturing relationships with their wards.
-        </p>
-        <p className="left">
-          • In upholding the schools ethos we have developed a behavior policy
-          with a set of clearly defined and rigorously enforced expectations
-          that are consistent. The end to this conduct strategy is to permit
-          students to foster self-restraint, mindfulness and thought for other
-          people. The policy also aims at helping students build intrinsic
-          value, a well implemented good behavior policy can also help students
-          to develop in ways that transcend the curriculum, equipping them for
-          life after school. We have zero tolerance for bullying and other vices
-          of indiscipline.
-        </p>
-        <p className="left">
-          • The counselling unit offers a range of support to students when need
-          arises.
-        </p>
-        <p className="left">
-          • School encourages graduating student of the senior category to study
-          and acquire their first degree in the United States (Canada) by
-          providing necessary information and aid needed to make this a dream
-          come through.
-        </p>
-        <p className="left">
-          Note: All financial requirements will be solely the responsibility of
-          the students’ parents/guardian. Every curricular activity is properly
-          curated to appeal to the passion and speculation of each student as
-          the school or parent sees fit.
-        </p>
-      </div>
+      <Flip bottom>
+        <h2>ABOUT INTOUCH PRIVATE ACADEMY</h2>
+      </Flip>
+      <Fade bottom delay={1000}>
+        <div>
+          <p>
+            Intouch Private Academy was established in January 2011 by the
+            schools proprietress and educationist, who is driven by the
+            compulsion to see younglings thrive and make a name for themselves.
+            At Intouch Private Academy every student is treated essentially as
+            an individual, with the potential for further excellence waiting to
+            be explored and refined. Our students are carefully guided through
+            their adolescence and taught to see mishaps and failures as success
+            inclined, provided that they learn from the negative experiences and
+            also to encourage the baby steps of progress taken by fellow
+            students likewise. This atmosphere encourages our students to
+            develop a lasting sense of moral, social and spiritual
+            responsibility. The aim is to turn out self-assured and caring
+            people through association and the co-activity of all school based
+            help administrations. The school likewise has prepared boarding
+            facilities to house and really focus on her boarding students:
+          </p>
+          <p className="left">
+            • House Parents are endowed with the responsibility of the wellbeing
+            of the students within the classroom and school context as well as
+            ensuring continuity of care and wellbeing of the wards, supported by
+            the resident medical staff, they also monitor assignments as well as
+            supervising evening weekend study time. They develop firm,
+            meaningful, nurturing relationships with their wards.
+          </p>
+          <p className="left">
+            • In upholding the schools ethos we have developed a behavior policy
+            with a set of clearly defined and rigorously enforced expectations
+            that are consistent. The end to this conduct strategy is to permit
+            students to foster self-restraint, mindfulness and thought for other
+            people. The policy also aims at helping students build intrinsic
+            value, a well implemented good behavior policy can also help
+            students to develop in ways that transcend the curriculum, equipping
+            them for life after school. We have zero tolerance for bullying and
+            other vices of indiscipline.
+          </p>
+          <p className="left">
+            • The counselling unit offers a range of support to students when
+            need arises.
+          </p>
+          <p className="left">
+            • School encourages graduating student of the senior category to
+            study and acquire their first degree in the United States (Canada)
+            by providing necessary information and aid needed to make this a
+            dream come through.
+          </p>
+          <p className="left">
+            Note: All financial requirements will be solely the responsibility
+            of the students’ parents/guardian. Every curricular activity is
+            properly curated to appeal to the passion and speculation of each
+            student as the school or parent sees fit.
+          </p>
+        </div>
+      </Fade>
     </div>
   );
 };
--- src/pages/OurActivities.jsx
+++ src/pages/OurActivities.jsx
@@ -7,65 +7,71 @@ const OurActivities = () => (
   <div className="OurActivities">
     <MainBanner title="Our Activities" />
     <div className="main-block">
-      <div className="block">
-        <img src={facility1} alt="" />
-        <div>
-          <h2>1. SCHOOLS EVENT AND CALENDAR</h2>
-          <ul>
-            <li>Cultural day</li>
-            <li>Independent day</li>
-            <li>Teacher’s day</li>
-            <li>Christmas carol</li>
-            <li>Valedictory services</li>
-            <li>Co-curricular activities</li>
-            <li>Creative and Cultural day</li>
-            <li>Mental Mathematics</li>
-          </ul>
+      <Bounce right>
+        <div className="block">
+          <img src={facility1} alt="" />
+          <div>
+            <h2>1. SCHOOLS EVENT AND CALENDAR</h2>
+            <ul>
+              <li>Cultural day</li>
+              <li>Independent day</li>
+              <li>Teacher’s day</li>
+              <li>Christmas carol</li>
+              <li>Valedictory services</li>
+              <li>Co-curricular activities</li>
+              <li>Creative and Cultural day</li>
+              <li>Mental Mathematics</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="block block--right">
-        <img src={facility1} alt="" />
-        <div>
-          <h2>ENTREPRENEURSHIP AND CO-CURRICULAR ACTIVITES</h2>
-          <p>
-            The Incorporation of entrepreneurship and co-curricular activities
-            help to enhance the intellectual, social and cultural development of
-            its members. These activities and societies help to harness talents
-            and leadership tendencies in students, to help them work as a team
-            and enhance national relevance. They also promote judicious use of
-            leisure, ethical character and corporate life of the boarding
-            system. The following is a list of entrepreneurship activities and
-            clubs managed by the school:
-          </p>
+      </Bounce>
+      <Bounce left delay={1000}>
+        <div className="block block--right">
+          <img src={facility1} alt="" />
+          <div>
+            <h2>ENTREPRENEURSHIP AND CO-CURRICULAR ACTIVITES</h2>
+            <p>
+              The Incorporation of entrepreneurship and co-curricular activities
+              help to enhance the intellectual, social and cultural development
+              of its members. These activities and societies help to harness
+              talents and leadership tendencies in students, to help them work
+              as a team and enhance national relevance. They also promote
+              judicious use of leisure, ethical character and corporate life of
+              the boarding system. The following is a list of entrepreneurship
+              activities and clubs managed by the school:
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="lists">
-        <div className="list">
-          <p className="title">ENTREPRENUERSHIP ACTIVITIES:</p>
-          <ul>
-            <li>Coding</li>
-            <li>Graphics Design</li>
-            <li>Music</li>
-            <li>Creative arts</li>
-            <li>Catering and Home management</li>
-            <li>Fashion illustration</li>
-            <li>Hair styling</li>
-          </ul>
+      </Bounce>
+      <Fade bottom delay={1300}>
+        <div className="lists">
+          <div className="list">
+            <p className="title">ENTREPRENUERSHIP ACTIVITIES:</p>
+            <ul>
+              <li>Coding</li>
+              <li>Graphics Design</li>
+              <li>Music</li>
+              <li>Creative arts</li>
+              <li>Catering and Home management</li>
+              <li>Fashion illustration</li>
+              <li>Hair styling</li>
+            </ul>
+          </div>
+          <div className="list list--right">
+            <p className="title">CO-CURRICULAR/CLUB ACTIVITIES:</p>
+            <ul>
+              <li>The Science club</li>
+              <li>The Arts Club</li>
+              <li>The Mental Mathematics club</li>
+              <li>The Music band</li>
+              <li>The Ballad Group</li>
+              <li>The Press Crew</li>
+              <li>The Chess Club</li>
+              <li>The Drama Club</li>
+            </ul>
+          </div>
         </div>
-        <div className="list list--right">
-          <p className="title">CO-CURRICULAR/CLUB ACTIVITIES:</p>
-          <ul>
-            <li>The Science club</li>
-            <li>The Arts Club</li>
-            <li>The Mental Mathematics club</li>
-            <li>The Music band</li>
-            <li>The Ballad Group</li>
-            <li>The Press Crew</li>
-            <li>The Chess Club</li>
-            <li>The Drama Club</li>
-          </ul>
-        </div>
-      </div>
+      </Fade>
     </div>
   </div>
 );
