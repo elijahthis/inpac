692e99a2db55b7ccc9d671747488359afab04e38
elijahthis <emperorelijah007.com>
2022-10-17 15:31:01 +0100

    testimonials

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -4,46 +4,47 @@ import "./styles.scss";
 import { Flip, Fade, Bounce } from "react-reveal";
 
 const Events = () => {
-  const eventList = [
-    {
-      img: facility1,
-      title: "Our 4th Inter- House Sport was a success!",
-      date: "April 15th, 2022.",
-      desc: "Lorem ipsum dolor sit am volutpat. Dolor, nec, molestie risus lee dictneque elit risus vivamus venenatis pellentesque. Blandit platea in arcu eget scelerisque pis ornare vulputate id phasellus. Ultrices velit donec scelerisque id iaculis amet. Vesti. Read more...",
-    },
-    {
-      img: facility1,
-      title: "Our 4th Inter- House Sport was a success!",
-      date: "April 15th, 2022.",
-      desc: "Lorem ipsum dolor sit am volutpat. Dolor, nec, molestie risus lee dictneque elit risus vivamus venenatis pellentesque. Blandit platea in arcu eget scelerisque pis ornare vulputate id phasellus. Ultrices velit donec scelerisque id iaculis amet. Vesti. Read more...",
-    },
-    {
-      img: facility1,
-      title: "Our 4th Inter- House Sport was a success!",
-      date: "April 15th, 2022.",
-      desc: "Lorem ipsum dolor sit am volutpat. Dolor, nec, molestie risus lee dictneque elit risus vivamus venenatis pellentesque. Blandit platea in arcu eget scelerisque pis ornare vulputate id phasellus. Ultrices velit donec scelerisque id iaculis amet. Vesti. Read more...",
-    },
-  ];
+	const eventList = [
+		{
+			img: facility1,
+			title: "Our 4th Inter- House Sport was a success!",
+			date: "April 15th, 2022.",
+			desc: "Lorem ipsum dolor sit am volutpat. Dolor, nec, molestie risus lee dictneque elit risus vivamus venenatis pellentesque. Blandit platea in arcu eget scelerisque pis ornare vulputate id phasellus. Ultrices velit donec scelerisque id iaculis amet. Vesti. ",
+		},
+		{
+			img: facility1,
+			title: "Our 4th Inter- House Sport was a success!",
+			date: "April 15th, 2022.",
+			desc: "Lorem ipsum dolor sit am volutpat. Dolor, nec, molestie risus lee dictneque elit risus vivamus venenatis pellentesque. Blandit platea in arcu eget scelerisque pis ornare vulputate id phasellus. Ultrices velit donec scelerisque id iaculis amet. Vesti.",
+		},
+		{
+			img: facility1,
+			title: "Our 4th Inter- House Sport was a success!",
+			date: "April 15th, 2022.",
+			desc: "Lorem ipsum dolor sit am volutpat. Dolor, nec, molestie risus lee dictneque elit risus vivamus venenatis pellentesque. Blandit platea in arcu eget scelerisque pis ornare vulputate id phasellus. Ultrices velit donec scelerisque id iaculis amet. Vesti.",
+		},
+	];
 
-  return (
-    <div className="Events">
-      <MainBanner title="News" />
-      <div className="main-block">
-        {eventList.map((eventItem, ind) => (
-          <Bounce right delay={300 * ind} key={ind}>
-            <div className="eventItem">
-              <img src={eventItem.img} alt="" srcset="" />
-              <div className="bulk">
-                <h3>{eventItem.title}</h3>
-                <button>{eventItem.date}</button>
-                <p>{eventItem.desc}</p>
-              </div>
-            </div>
-          </Bounce>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="Events">
+			<MainBanner title="News" />
+			<div className="main-block">
+				{eventList.map((eventItem, ind) => (
+					<Bounce right delay={300 * ind} key={ind}>
+						<div className="eventItem">
+							<img src={eventItem.img} alt="" srcset="" />
+							<div className="bulk">
+								<h3>{eventItem.title}</h3>
+								<button>{eventItem.date}</button>
+								<p>{eventItem.desc}</p>
+								<span>Read more...</span>
+							</div>
+						</div>
+					</Bounce>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Events;
--- src/pages/OurActivities.jsx
+++ src/pages/OurActivities.jsx
@@ -1,79 +1,80 @@
 import { MainBanner } from "../components/MainBanner";
-import facility1 from "../assets/images/facility-1.png";
+import activity1 from "../assets/images/activity-1.png";
+import activity2 from "../assets/images/activity-2.png";
 import "./styles.scss";
 import { Flip, Fade, Bounce } from "react-reveal";
 
 const OurActivities = () => (
-  <div className="OurActivities">
-    <MainBanner title="Our Activities" />
-    <div className="main-block">
-      <Bounce right>
-        <div className="block">
-          <img src={facility1} alt="" />
-          <div>
-            <h2>1. SCHOOLS EVENT AND CALENDAR</h2>
-            <ul>
-              <li>Cultural day</li>
-              <li>Independent day</li>
-              <li>Teacher’s day</li>
-              <li>Christmas carol</li>
-              <li>Valedictory services</li>
-              <li>Co-curricular activities</li>
-              <li>Creative and Cultural day</li>
-              <li>Mental Mathematics</li>
-            </ul>
-          </div>
-        </div>
-      </Bounce>
-      <Bounce left delay={1000}>
-        <div className="block block--right">
-          <img src={facility1} alt="" />
-          <div>
-            <h2>ENTREPRENEURSHIP AND CO-CURRICULAR ACTIVITES</h2>
-            <p>
-              The Incorporation of entrepreneurship and co-curricular activities
-              help to enhance the intellectual, social and cultural development
-              of its members. These activities and societies help to harness
-              talents and leadership tendencies in students, to help them work
-              as a team and enhance national relevance. They also promote
-              judicious use of leisure, ethical character and corporate life of
-              the boarding system. The following is a list of entrepreneurship
-              activities and clubs managed by the school:
-            </p>
-          </div>
-        </div>
-      </Bounce>
-      <Fade bottom delay={1300}>
-        <div className="lists">
-          <div className="list">
-            <p className="title">ENTREPRENUERSHIP ACTIVITIES:</p>
-            <ul>
-              <li>Coding</li>
-              <li>Graphics Design</li>
-              <li>Music</li>
-              <li>Creative arts</li>
-              <li>Catering and Home management</li>
-              <li>Fashion illustration</li>
-              <li>Hair styling</li>
-            </ul>
-          </div>
-          <div className="list list--right">
-            <p className="title">CO-CURRICULAR/CLUB ACTIVITIES:</p>
-            <ul>
-              <li>The Science club</li>
-              <li>The Arts Club</li>
-              <li>The Mental Mathematics club</li>
-              <li>The Music band</li>
-              <li>The Ballad Group</li>
-              <li>The Press Crew</li>
-              <li>The Chess Club</li>
-              <li>The Drama Club</li>
-            </ul>
-          </div>
-        </div>
-      </Fade>
-    </div>
-  </div>
+	<div className="OurActivities">
+		<MainBanner title="Our Activities" />
+		<div className="main-block">
+			<Bounce right>
+				<div className="block">
+					<div>
+						<h2>1. SCHOOLS EVENT AND CALENDAR</h2>
+						<ul>
+							<li>Cultural day</li>
+							<li>Independent day</li>
+							<li>Teacher’s day</li>
+							<li>Christmas carol</li>
+							<li>Valedictory services</li>
+							<li>Co-curricular activities</li>
+							<li>Creative and Cultural day</li>
+							<li>Mental Mathematics</li>
+						</ul>
+					</div>
+					<img src={activity1} alt="" />
+				</div>
+			</Bounce>
+			<Bounce left delay={1000}>
+				<div className="block block--right">
+					<div>
+						<h2>ENTREPRENEURSHIP AND CO-CURRICULAR ACTIVITES</h2>
+						<p>
+							The Incorporation of entrepreneurship and co-curricular activities
+							help to enhance the intellectual, social and cultural development
+							of its members. These activities and societies help to harness
+							talents and leadership tendencies in students, to help them work
+							as a team and enhance national relevance. They also promote
+							judicious use of leisure, ethical character and corporate life of
+							the boarding system. The following is a list of entrepreneurship
+							activities and clubs managed by the school:
+						</p>
+					</div>
+					<img src={activity2} alt="" />
+				</div>
+			</Bounce>
+			<Fade bottom delay={1300}>
+				<div className="lists">
+					<div className="list">
+						<p className="title">ENTREPRENUERSHIP ACTIVITIES:</p>
+						<ul>
+							<li>Coding</li>
+							<li>Graphics Design</li>
+							<li>Music</li>
+							<li>Creative arts</li>
+							<li>Catering and Home management</li>
+							<li>Fashion illustration</li>
+							<li>Hair styling</li>
+						</ul>
+					</div>
+					<div className="list list--right">
+						<p className="title">CO-CURRICULAR/CLUB ACTIVITIES:</p>
+						<ul>
+							<li>The Science club</li>
+							<li>The Arts Club</li>
+							<li>The Mental Mathematics club</li>
+							<li>The Music band</li>
+							<li>The Ballad Group</li>
+							<li>The Press Crew</li>
+							<li>The Chess Club</li>
+							<li>The Drama Club</li>
+						</ul>
+					</div>
+				</div>
+			</Fade>
+		</div>
+	</div>
 );
 
 export default OurActivities;
