1b7d80fc548407e17465e8be7c32cafaa911a545
elijahthis <emperorelijah007.com>
2022-10-19 14:23:07 +0100

    gallery show more

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,34 +1,25 @@
 import { MainBanner } from "../components/MainBanner";
-import facility1 from "../assets/images/facility-1.png";
-import facility2 from "../assets/images/facility-2.png";
-import facility3 from "../assets/images/facility-3.png";
-import facility4 from "../assets/images/facility-4.png";
-import facility5 from "../assets/images/facility-5.png";
-import facility6 from "../assets/images/facility-6.png";
+
 import gallery1 from "../assets/images/gallery-1.png";
 import gallery2 from "../assets/images/gallery-2.png";
 import gallery3 from "../assets/images/gallery-3.png";
 import gallery4 from "../assets/images/gallery-4.png";
 import gallery5 from "../assets/images/gallery-5.png";
 import gallery6 from "../assets/images/gallery-6.png";
+import gallery11 from "../assets/images/gallery-11.png";
+import gallery12 from "../assets/images/gallery-12.png";
+import gallery13 from "../assets/images/gallery-13.png";
+import gallery14 from "../assets/images/gallery-14.png";
+import gallery15 from "../assets/images/gallery-15.png";
 import "./styles.scss";
 import { Flip, Fade, Bounce } from "react-reveal";
+import { useState } from "react";
 
 const Gallery = () => {
-	const images = [
-		facility1,
-		facility2,
-		facility3,
-		facility4,
-		facility5,
-		facility6,
-		gallery1,
-		gallery2,
-		gallery3,
-		gallery4,
-		gallery5,
-		gallery6,
-	];
+	const [showMore, setShowMore] = useState(false);
+
+	const images = [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6];
+	const moreImages = [gallery11, gallery2, gallery13, gallery14, gallery15];
 
 	return (
 		<div className="Gallery">
@@ -59,9 +50,20 @@ const Gallery = () => {
 							</Bounce>
 						))}
 					</div>
+					{showMore && (
+						<div>
+							{moreImages.map((image, ind) => (
+								<Bounce right delay={500 + 300 * ind} key={ind}>
+									<img src={image} />
+								</Bounce>
+							))}
+						</div>
+					)}
 					<div className="button-wrap">
 						<Fade bottom delay={1000}>
-							<button>View More</button>
+							<button onClick={() => setShowMore(!showMore)}>
+								{showMore ? "Show less" : "View More"}
+							</button>
 						</Fade>
 					</div>
 				</div>
